4577d7a4540f540b8fbd82d99ef8e42739d38bcf
Sylvester Zowonu <szowonu@hotmail.com>
2016-07-22 18:11:52 -0700

    Add project files.

---
+++ UTF_8_Bom2UTF_8/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UTF_8_Bom2UTF_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
